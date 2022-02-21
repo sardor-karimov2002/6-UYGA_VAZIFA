84518600519ba02d0215fc90f3986d9af39be8a4
sardor-karimov2002 <sardorbekkarimov155@gmail.com>
2022-02-22 00:45:56 +0500

    sardor karimov

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+const  dollir=9433.34 ;
+const yevro=10354.03;
+
+var yulKira=Number(500*dollir);
+var meh=Number(250*dollir);
+var muzey=Number(120*yevro);
+var jamiSumma=Number(prompt("Alisher qancha pulingiz bor"))
+
+
+if(jamiSumma>=(yulKira+meh+muzey)){
+    console.log("oq yul")
+    let balance=Number(prompt("oq yul"))
+}else{
+    console.log("sabr")
+    let balance=Number(prompt("sabr"))
+}
+
+
+
